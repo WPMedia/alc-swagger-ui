2e08bc044ff75639514a55adfd6aeb376db39aab
sb-rlje <Skylar.Banks@washpost.com>
2019-04-04 16:14:19 -0400

    Add arc nav to api docs template

--- src/plugins/topbar/topbar.jsx
+++ src/plugins/topbar/topbar.jsx
@@ -1,45 +1,45 @@
 import React from 'react'
-import Logo from './arc-logo_white.svg'
+
+const { ArcNav } = window
 
 const Topbar = () => {
-  return (
-    <div className='navbar arc-navbar' id="topofpage">
-      <div className='navbar-header'>
-        <button
-          type='button'
-          className='navbar-toggle'
-          data-toggle='collapse'
-          data-target='#navCollapse'
-          aria-expanded='false'
-          aria-label='Toggle navigation'>
-          <span className='sr-only'>Toggle navigation</span>
-          <span className='icon-bar' />
-          <span className='icon-bar' />
-          <span className='icon-bar' />
-        </button>
-        <div>
-          <a href='/'>
-            <img className='arc-logo' src={Logo} alt='Arc Logo' />
-          </a>
-          <a href='/alc'>
-            <div className='navbar-brand'>
-              <h4>Learning Center</h4>
-            </div>
-          </a>
-        </div>
-      </div>
-      <div className='collapse navbar-collapse' id='navCollapse'>
-        <ul className='nav navbar-nav'>
-          <li className='menu-item'>
-            <a href='/answers'> arc answers</a>
-          </li>
-          <li className='menu-item'>
-            <a href='https://arcpublishing.atlassian.net/servicedesk/customer/portal/2'> contact support </a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  )
+  return ArcNav ? (
+    <ArcNav
+      appname='Arc'
+      homeLink='/alc'
+      links={[
+        {
+          "name": "Arc Answers",
+          "href": "/alc/answers/",
+          "text": {
+            "en": "Arc Answers"
+          }
+        },
+        {
+          "name": "Release Notes",
+          "href": "/alc/release-notes",
+          "text": {
+            "en": "Release Notes"
+          }
+        },
+        {
+          "name": "Support",
+          "href": "https://arcpublishing.atlassian.net/servicedesk",
+          "text": {
+            "en": "Support"
+          }
+        },
+        {
+          "name": "Ideas",
+          "href": "https://ideas.arcpublishing.com/",
+          "text": {
+            "en": "Ideas"
+          }
+        }
+      ]}
+    />
+  ) : null
 }
 
 export default Topbar
+
